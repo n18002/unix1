253344dd21da91b017cb9583da5e4753179f6972
Hiroya Inafuku <n18002@std.it-college.ac.jp>
2018-05-23 04:41:51 +0000

    5/23の課題

--- 0523/052303.sh
+++ 0523/052303.sh
@@ -1,1 +1,1 @@
-rgs du -b | sort -n | head -n 5
+find ~ -type f | xargs du -b | sort -n | head -n 5 
--- 0523/052304.sh
+++ 0523/052304.sh
@@ -1,1 +1,1 @@
-udo find /etc/ -type f | sudo xargs du -b | sort -n | tac | head -n 5
+sudo du -b /etc/* | sort -n | tac | head -n 5
