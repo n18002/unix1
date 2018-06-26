007da7baf96906a5f34d076317691756ab0d4619
Hiroya Inafuku <n18002@std.it-college.ac.jp>
2018-06-26 04:43:09 +0000

    6/14の修正

--- 0614/0614.sh
+++ 0614/0614.sh
@@ -6,8 +6,7 @@ i400=`expr $in % 400`
 ans=`expr $i4 == 0 \& $i100 \!= 0 \| $i400 == 0`
 if [ $ans != 0 ]
 then
-		echo "閏年"
-	else
-			echo "閏年ではない"
-		fi
+				echo "閏年"
+else
+				echo "閏年ではない"
 fi
