5fc587636575e037946c240fe801f2ef6d5f3f99
Hiroya Inafuku <n18002@std.it-college.ac.jp>
2018-06-26 04:36:39 +0000

    6/20の修正

--- 0620/0620.sh
+++ 0620/0620.sh
@@ -1,15 +1,17 @@
-for i in `seq 1 100`; do	
-				
-				if [ $(($i % 3)) == 0 -a $(($i % 5)) == 0 ];then
+for i in `seq 1 100`; do
+
+				if [ $(($i % 3)) == 0 -a $(($i % 5)) == 0 ]; then
 												echo "FIZZBUZZ"
-		
+
 				elif [ $(($i % 3)) == 0 ]; then
 									echo "FIZZ"
-		
+
 				elif [ $(($i % 5)) == 0 ]; then
 												echo "BUZZ"
-				
+
 				else
-												echo "$i"
+												echo "$1"
+
 				fi
+
 done
